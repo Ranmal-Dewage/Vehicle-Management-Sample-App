dd5d3bd22a282c6dd5dacaf942aeb520c43fbd93
Ranmal-Dewage <ranmal.b.dewage@gmail.com>
2021-06-25 23:49:12 +0530

    Websocket Server Completed

--- websocket-server/src/queue/message.consumer.ts
+++ websocket-server/src/queue/message.consumer.ts
@@ -17,9 +17,31 @@ export class MessageConsumer {
         let client: Socket = this.clientsArray.find((storedClient) => {
             return storedClient.id === job.data.clientId
         });
-        let filteredVehicles: Vehicle[] = await this.appService.getVehcilesByAge(job.data.data);
-        console.log(filteredVehicles);
-        client.emit('complete', "You Send Value : " + job.data.data)
+
+        try {
+            let filteredVehicles: Vehicle[] = await this.appService.getVehcilesByAge(job.data.data);
+            const csvRows = this.getCSVRowData(filteredVehicles);
+            let fileData = {
+                name: "Vehicles with age greater than or equal to " + job.data.data + " Years.csv",
+                blob: csvRows
+            }
+            client.emit('complete', fileData)
+
+        } catch (error) {
+            client.emit('errorComplete', error.message)
+        }
+
+    }
+
+    private getCSVRowData(vehicles: Vehicle[]) {
+        const csvData = [];
+        const headers = "id, first_name, last_name, email, car_make, car_model, vin_number, manufactured_date, age_of_vehcile";
+        csvData.push(headers);
+        for (const vehicle of vehicles) {
+            csvData.push(vehicle.id + ',' + vehicle.firstName + ',' + vehicle.lastName + ',' + vehicle.email + ',' + vehicle.carMake
+                + ',' + vehicle.carModel + ',' + vehicle.vin + ',' + vehicle.manufacturedDate + ',' + vehicle.ageOfVehicle);
+        }
+        return csvData.join("\n")
     }
 
 }
